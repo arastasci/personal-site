296f9190f52b36a896977c3a62a976bc9bd749b4
arastasci <arastasci@gmail.com>
2025-02-12 00:58:49 +0300

    set base config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   site: "https://arastasci.github.io/personal-site/",
-  base: "personal-site"
+  base: "/personal-site/",
   integrations: [mdx(), sitemap(), tailwind()],
 });
